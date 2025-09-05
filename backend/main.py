21a23ac9d08339e24541e167cf8c70d566b73730
Jiyansh-2006 <jiyanshkushwahmain@gmail.com>
2025-09-05 09:35:46 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,8 @@ app = FastAPI()
 # Allow frontend requests
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://loansimp-lify.onrender.com"],  # you can restrict to your frontend URL later
+    allow_origins=["https://loansimp-lify.onrender.com",
+        "https://loan-simp-lify.onrender.com"],  # you can restrict to your frontend URL later
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -248,4 +249,5 @@ async def reset_verification():
     active_user = None
     otp_store = {}
     return {"message":"Verification system reset. Next person can verify now."}
+
 
